e72f573d518d86ecbf34c843736ab1865504d363
h.higuchi.93@gmail.com <h.higuchi.93@gmail.com>
2023-02-19 23:11:17 +0900

    デプロイ関数修正

--- contract/scripts/deployMetaplantsFree.js
+++ contract/scripts/deployMetaplantsFree.js
@@ -1,17 +1,29 @@
+const CONTRACT_NAME = "Metaplants Pass";
+const CONTRACT_SYMBOL = "MPLANTSPASS";
+
 const main = async () => {
-    const nftContractFactory = await hre.ethers.getContractFactory("MetaplantsFree");
-    const nftContract = await nftContractFactory.deploy();
-    await nftContract.deployed();
-    console.log("Contract deployed to:", nftContract.address);
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
-  runMain();
+  const nftContractFactory = await hre.ethers.getContractFactory(
+    "MetaplantsFree"
+  );
+  const nftContract = await nftContractFactory.deploy(
+    CONTRACT_NAME,
+    CONTRACT_SYMBOL
+  );
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+  await hre.run("verify:verify", {
+    address: nftContract.address,
+    constructorArguments: [CONTRACT_NAME, CONTRACT_SYMBOL],
+  });
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+runMain();
