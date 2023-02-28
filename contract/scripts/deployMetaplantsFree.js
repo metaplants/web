e4c24adb369ec9c2c677c7d050eca11962b25623
h.higuchi.93@gmail.com <h.higuchi.93@gmail.com>
2023-02-28 19:13:04 +0900

    deployスクリプトにverifyするまでの待ち時間追加

--- contract/scripts/deployMetaplantsFree.js
+++ contract/scripts/deployMetaplantsFree.js
@@ -1,5 +1,5 @@
-const CONTRACT_NAME = "Metaplants Pass";
-const CONTRACT_SYMBOL = "MPLANTSPASS";
+const CONTRACT_NAME = "Metaplants";
+const CONTRACT_SYMBOL = "MPT";
 
 const main = async () => {
   const nftContractFactory = await hre.ethers.getContractFactory(
@@ -11,6 +11,7 @@ const main = async () => {
   );
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
+  await new Promise((resolve) => setTimeout(resolve, 15 * 1000)); // wait 15 seconds until contract is verifiable on Explorer
   await hre.run("verify:verify", {
     address: nftContract.address,
     constructorArguments: [CONTRACT_NAME, CONTRACT_SYMBOL],
